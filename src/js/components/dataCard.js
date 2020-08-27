b754ae083d0973d95fe928fd1cbf2a5a829cad35
Victor <j7fernandes@gmail.com>
2020-08-27 20:34:48 -0300

    Core

--- src/js/components/dataCard.js
+++ src/js/components/dataCard.js
@@ -1,13 +1,14 @@
 export class DataCard extends HTMLElement {
 
-    set data({ image, name, description }) {
+    set data({ image, nome, telefone, inauguracao }) {
         this.innerHTML = `
         <div class="card mx-4 mb-4">
-            <img class="card-img-top  rounded mx-auto d-block card-image mt-4" src="${ image}" alt="Card image cap">
+            <img class="card-img-top  rounded mx-auto d-block card-image mt-4" src="${ image || `https://www.tibs.org.tw/images/default.jpg`}" alt="Card image cap">
             <div class="card-body">
-                <h3 class="card-title text-center text-bold">${name}</h3>
-                <p class="card-text">${description}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h3 class="card-title text-center text-bold">${nome}</h3>
+                <p class="card-text">Inaugurado em ${inauguracao}</p>
+                <p class="card-text">Telefone: ${telefone}</p>
+                <a href="#" class="btn btn-primary">Favourite</a>
             </div>
         </div>
         `;
--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,55 @@
 import { DataCard } from './components/dataCard.js';
 
 
+const searchBar = document.querySelector(`#searchBar`);
+
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9hb3Z4Y3RvciIsImEiOiJja2UzZnNuMmswaXliMnJ1bGt5eTJycndpIn0.ahHfnxxfs1L2OYlTsxKlcQ';
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  center: [-47.0616, -22.9064], // starting position [lng, lat]
+  zoom: 12 // starting zoom
 });
 
 
-const searchBar = document.querySelector(`#searchBar`);
-let data;
 
 
 
 searchBar.addEventListener(`keyup`, async (e) => {
   const searchString = e.target.value?.toLowerCase();
 
-  data = searchString ? data.filter(d => d.name.toLowerCase().includes(searchString)) : await getData();
+  const data = await getData();
 
-  displayData();
+  displayData(searchString ? data.filter(d => d.nome.toLowerCase().includes(searchString)) : data);
 });
 
 const getData = async () => {
-  const res = await fetch('./assets/data.json');
+  const res = await fetch('./assets/datahospitais.json');
   return await res.json();
 };
 
 
-async function displayData() {
+const displayData = async (data) => {
   const main = document.querySelector(`main`);
   main.innerHTML = ``;
 
 
-  data.forEach(data => {
+  data.forEach(hospital => {
     const el = document.createElement(`data-card`);
-    el.data = data;
+    el.data = hospital;
     main.appendChild(el);
+
+    // const marker = new mapboxgl.Marker().setLngLat([hospital.endereco.longitude, hospital.endereco.latitude])
+    //   .setPopup(new mapboxgl.Popup().setHTML(el.innerHTML))
+
+    //   .addTo(map);
   });
-}
+};
 
 document.addEventListener(`DOMContentLoaded`, async () => {
-  data = await getData();
-  await displayData();
+
+  await displayData(await getData());
+  const a = document.querySelector(`.carousel-inner`);
+
 });
+
